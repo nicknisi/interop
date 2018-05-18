86de1d943450e13fc0720aa49f066e10ed24c5e6
Nick Nisi <nick@nisi.org>
2018-05-18 09:46:51 -0500

    basic example dgrid working through wrapper

--- src/dgrid/DgridWrapper.ts
+++ src/dgrid/DgridWrapper.ts
@@ -2,17 +2,29 @@ import { v } from '@dojo/widget-core/d';
 import { VNode } from '@dojo/widget-core/interfaces';
 import { WidgetBase } from '@dojo/widget-core/WidgetBase';
 import afterRender from '@dojo/widget-core/decorators/afterRender';
-import Base from '@dojo/widget-core/meta/Base';
-import * as dgrid from 'dgrid';
-import * as dstore from 'dojo-dstore';
+import MetaBase from '@dojo/widget-core/meta/Base';
+
+import declare = require('dojo/_base/declare');
+import List = require('dgrid/List');
+import OnDemandList = require('dgrid/OnDemandList');
+import Grid = require('dgrid/Grid');
+import OnDemandGrid = require('dgrid/OnDemandGrid');
+import Pagination = require('dgrid/extensions/Pagination');
+import MemoryStore = require('dojo-dstore/Memory');
 
 /**
  * An internal meta provider that provides the rendered DOM node on _root_ Dijits
  */
-class DomNode extends Base {
-	public get(key: string | number) {
+class DomNode extends MetaBase {
+	get(key: string | number) {
 		return this.getNode(key);
 	}
+}
+
+export enum GridType {
+	List,
+	Grid,
+	PaginatedGrid
 }
 
 /**
@@ -21,8 +33,20 @@ class DomNode extends Base {
 const DEFAULT_KEY = 'root';
 
 export interface DgridWrapperProperties<T> {
+	type: GridType;
 	data: T[];
-	columns: {}[];
+	columns: { [columnName: string]: string };
+}
+
+function getGridBase(gridType: GridType) {
+	switch (gridType) {
+		case GridType.PaginatedGrid:
+			return declare([Grid, Pagination]);
+		case GridType.List:
+			return OnDemandList;
+		case GridType.Grid:
+			return OnDemandGrid;
+	}
 }
 
 /**
@@ -31,12 +55,13 @@ export interface DgridWrapperProperties<T> {
  * @param tagName The tag name that should be used when creating the DOM for the dijit. Defaults to `div`.
  */
 class DgridWrapper<T = {}> extends WidgetBase<DgridWrapperProperties<T>> {
-	private _grid: dgrid.Pagination<T> | undefined;
+	private _grid: Pagination<T> | undefined;
 	private _node: HTMLElement | undefined;
+	private _store: MemoryStore | undefined;
 
 	private _updateGrid(data: T[]) {
 		// not null assertion, because this can only be called when `_dijit` is assigned
-		this._grid!.setData(data);
+		this._store!.setData(data);
 	}
 
 	protected render(): VNode {
@@ -53,15 +78,19 @@ class DgridWrapper<T = {}> extends WidgetBase<DgridWrapperProperties<T>> {
 	}
 
 	@afterRender()
-	public initGrid() {
-		this._node = this.meta(DomNode).get('root') as HTMLElement;
-		this._grid = new dgrid.Pagination(
-			{
-				collection: new dstore.MemoryStore({ data: this.properties.data }),
-				columns: this.properties.columns
-			},
-			this._node
+	public initGrid(result: VNode) {
+		const { key = DEFAULT_KEY, data, columns, type: gridType } = this.properties;
+		const node = this._node = this.meta(DomNode).get(key) as HTMLElement;
+		const collection = this._store = new MemoryStore({ data });
+		const Grid = getGridBase(gridType);
+		this._grid = new Grid(
+			{ collection, columns },
+			node
 		);
+
+		console.log('_grid', this._grid);
+
+		return result;
 	}
 }
 
